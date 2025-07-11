1af44e4c80679aa924dbbe5a80a47d96f0cd6c5d
joão victor <joao.nogueira@dcx.ufpb.br>
2025-07-11 17:32:49 -0300

    Add files via upload

---
+++ list compression.py
@@ -0,0 +1,20 @@
+um=[i*1 for i in range(1,11)]
+print("A tabuada do 1:",um)
+dobro=[i*2 for i in range(1,10)]
+print("A tabuada do 2: ",dobro)
+triplo=[i*3 for i in range(1,11)]
+print("A tabuada do 3: ",triplo)
+quatro=[i*4 for i in range(1,11)]
+print("A tabuada do 4: ",quatro)
+cinco=[i*5 for i in range(1,11)]
+print("A tabuada do 5: ",cinco)
+seis=[i*6 for i in range(1,11)]
+print("A tabuada do 6: ",seis)
+sete=[i*7 for i in range(1,11)]
+print("A tabuada do 7:",sete)
+oito=[i*8 for i in range(1,11)]
+print("A tabuada dos 8:",oito)
+nove=[i*9 for i in range(1,11)]
+print("A tabuada do 9: ",nove)
+dez=[i*10 for i in range(1,11)]
+print("A tabuada do 10: ",dez)
